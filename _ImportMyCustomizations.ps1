a2a6d20c35e29734474bd6d68387585185cfede1
Amr Eldib <amr@amreldib.com>
2018-08-12 20:35:15 -0700

    Updated Import script to be able to run from another script

--- _ImportMyCustomizations.ps1
+++ _ImportMyCustomizations.ps1
@@ -1,13 +1,15 @@
-$customizationsFile = "_MyCustomizations.txt"
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$customizationsFile = Join-Path $scriptPath "_MyCustomizations.txt" 
 
 if (Test-Path $customizationsFile) { 
-    foreach($line in Get-Content $customizationsFile) {
-        if ($line.EndsWith(".reg")) {
-            Invoke-Expression 'reg import "$line"'
-            Write-Output "Imported '$line'"
-        }
-    }
+	foreach ($line in Get-Content $customizationsFile) {
+		if ($line.EndsWith(".reg")) {
+			$file = Join-Path $scriptPath .\$line
+			Invoke-Expression 'reg import "$file"'
+			Write-Output "Imported '$line'"
+		}
+	}
 }
 else {
-    Write-Output("Couldn't find _MyCustomizations.txt file")
+	Write-Output("Couldn't find _MyCustomizations.txt file")
 }
